04b1dc42d22afcee1c184bef5a9fa4c17920ca9a
Melokuhle <melokuhlemaphisa99@gmail.com>
2025-08-12 15:53:52 +0200

    printed my name using list array

---
+++ task-manger.js
@@ -0,0 +1,13 @@
+let process = require('process'); //import node module
+let nextId = 1;
+let tasks = []
+
+console.log(process.argv);
+
+console.log(process.argv[5])
+
+
+// function createTask(Id, TaskName) {
+
+
+// }
